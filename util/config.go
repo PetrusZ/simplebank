d3a2062fec4223408528e8326696abc649dc470a
PetrusZ <silencly07@gmail.com>
2022-11-09 01:41:45 +0800

    feat(gapi): Write structured logs for gRPC APIs
    Signed-off-by: PetrusZ <silencly07@gmail.com>

--- util/config.go
+++ util/config.go
@@ -7,8 +7,9 @@ import (
 )
 
 // Config stores all configuration of the application.
-// The values are read by vipper from a config file or enviroment variables.
+// The values are read by vipper from a config file or environment variables.
 type Config struct {
+	Environment          string        `mapstructure:"ENVIRONMENT"`
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
