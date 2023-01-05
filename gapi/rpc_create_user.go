782c89f17c409223a55867aee6d406f73db7e3f9
PetrusZ <silencly07@gmail.com>
2023-01-06 02:43:51 +0800

    feat(worker): Integrate async worker to web server

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -2,11 +2,14 @@ package gapi
 
 import (
 	"context"
+	"time"
 
 	db "github.com/PetrusZ/simplebank/db/sqlc"
 	"github.com/PetrusZ/simplebank/pb"
 	"github.com/PetrusZ/simplebank/util"
 	"github.com/PetrusZ/simplebank/val"
+	"github.com/PetrusZ/simplebank/worker"
+	"github.com/hibiken/asynq"
 	"github.com/lib/pq"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
@@ -42,7 +45,19 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
 
-	// Send verify email to user
+	// TODO: use db transaction
+	taskPayload := &worker.PayloadSendVerifyEmail{
+		Username: user.Username,
+	}
+	opts := []asynq.Option{
+		asynq.MaxRetry(10),
+		asynq.ProcessIn(10 * time.Second),
+		asynq.Queue(worker.QueueCritical),
+	}
+	err = server.taskDistributor.DistributorSendVerifyEmail(ctx, taskPayload, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to distributor task to send verify email: %s", err)
+	}
 
 	rsp := &pb.CreateUserResponse{
 		User: convertUser(user),
