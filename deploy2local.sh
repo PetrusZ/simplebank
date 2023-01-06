7153f02db92c4a3afd16ee67450727fba5611e3f
PetrusZ <silencly07@gmail.com>
2023-01-06 19:12:48 +0800

    feat(config): add REDIS_DB

--- deploy2local.sh
+++ deploy2local.sh
@@ -4,10 +4,12 @@ set -o pipefail
 
 DB_SOURCE=postgresql://local:local_secret07@home.codeplayer.org:15432/simple_bank_local?sslmode=disable
 REDIS_ADDRESS=redis-master.codeplayer.org:6379
+REDIS_DB=1
 TAG=local
 
 ORIG_DB_SOURCE=`grep DB_URL= Makefile | awk -F '=' '{print $2FS$3}'`
 ORIG_REDIS_ADDRESS=`grep REDIS_ADDRESS= app.env | awk -F '=' '{print $2$3}'`
+ORIG_REDIS_DB=`grep REDIS_DB= app.env | awk -F '=' '{print $2$3}'`
 ORIG_TAG=`grep image: k8s/deployment.yaml | awk -F ':' '{print $3}'`
 
 # Change config
@@ -15,6 +17,7 @@ changeConfig() {
     gsed -i "s#DB_URL=.*#DB_URL=${DB_SOURCE}#" Makefile
     gsed -i "s#DB_SOURCE=.*#DB_SOURCE=${DB_SOURCE}#" app.env
     gsed -i "s#REDIS_ADDRESS=.*#REDIS_ADDRESS=${REDIS_ADDRESS}#" app.env
+    gsed -i "s#REDIS_DB=.*#REDIS_DB=${REDIS_DB}#" app.env
     gsed -i "s#ENVIRONMENT=.*#ENVIRONMENT=production#" app.env
     gsed -i "s#image: patrickz07/simple-bank:.*#image: patrickz07/simple-bank:${TAG}#" k8s/deployment.yaml
 }
@@ -24,6 +27,7 @@ restoreConfig() {
     gsed -i "s#DB_URL=.*#DB_URL=${ORIG_DB_SOURCE}#" Makefile
     gsed -i "s#DB_SOURCE=.*#DB_SOURCE=${ORIG_DB_SOURCE}#" app.env
     gsed -i "s#REDIS_ADDRESS=.*#REDIS_ADDRESS=${ORIG_REDIS_ADDRESS}#" app.env
+    gsed -i "s#REDIS_DB=.*#REDIS_DB=${ORIG_REDIS_DB}#" app.env
     gsed -i "s#ENVIRONMENT=.*#ENVIRONMENT=development#" app.env
     gsed -i "s#image: patrickz07/simple-bank:.*#image: patrickz07/simple-bank:${ORIG_TAG}#" k8s/deployment.yaml
 }
